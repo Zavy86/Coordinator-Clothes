b83bd9325e5ec66f765e87530d9e29b3f47d2d9e
Manuel Zavatta <manuel.zavatta@gmail.com>
2018-04-03 18:09:47 +0200

    Module information

---
+++ module.inc.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Clothes
+ *
+ * @package Coordinator\Modules\Clothes
+ * @author  Manuel Zavatta <manuel.zavatta@gmail.com>
+ * @link    http://www.zavynet.org
+ */
+ $module_name="clothes";
+ $module_repository_version_url="https://raw.githubusercontent.com/Zavy86/Coordinator-Clothes/master/VERSION.txt";
+?>
